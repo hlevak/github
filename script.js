dad097cc52e8542ebede508264c1cf7ee63c552a
user_dever <example@gmail.com>
2020-08-26 16:10:19 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,47 @@
+"use strict";
+
+/* 
+console.log('arr' + " - object");
+console.log(4 + +"5");
+ */
+
+
+let incr = 10,
+    decr = 10;
+
+incr++;
+decr--;
+
+console.log (incr);
+console.log (decr);
+console.log ();
+
+
+/* 
+const category = 'toys';
+
+console.log (`https//someurl.com/${category}/5`);
+
+const user = 'Ivan';
+
+alert (`Даров, ${user}`); */
+
+
+// alert("Hello");
+
+// let result = confirm ("Are you here?");
+// console.log(result);
+
+// const answer = +prompt ("Вам есть 18?", "18");
+// console.log ( answer, "type: ", typeof (answer) );
+
+// const answers = [];
+
+// // answers [0] = prompt ( "Имя?", "" );
+// // answers [1] = prompt ( "Фамилия?", "" );
+// // answers [2] = prompt ( "Лет?", "" );
+
+// console.log ( typeof (answers) );
+// console.log ( typeof (null) );
+// console.log ( typeof (Infinity) );
+// console.log ( typeof (NaN) );
